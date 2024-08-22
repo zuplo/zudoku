59604270906ea74177aefcd73e5cee9fd671e3ca
Dan <dan@zuplo.com>
2024-08-22 11:50:00 +0200

    Update and sync examples + logos (#17)

--- examples/with-auth0/zudoku.config.ts
+++ examples/with-auth0/zudoku.config.ts
@@ -1,64 +1,37 @@
-import type { ZudokuConfig } from "zudoku";
-import navigation from "./navigation.js";
+import { type ZudokuConfig } from "zudoku";
 
 const config: ZudokuConfig = {
-  page: {
-    pageTitle: "",
-    logoUrl: "https://cdn.zudoku.dev/logos/icon.svg",
-    logo: {
-      src: {
-        light: "https://cdn.zudoku.dev/logos/zudoku-logo-full-light.svg",
-        dark: "https://cdn.zudoku.dev/logos/zudoku-logo-full-dark.svg",
+  topNavigation: [
+    { id: "documentation", label: "Documentation" },
+    { id: "api", label: "Rick & Morty API" },
+  ],
+  sidebar: {
+    documentation: [
+      {
+        type: "category",
+        label: "Get started",
+        items: ["introduction", "installation"],
       },
-      width: "130px",
-    },
+    ],
   },
-  metadata: {
-    title: "%s | Zudoku",
+  redirects: [{ from: "/", to: "/documentation/introduction" }],
+  docs: {
+    files: "/pages/**/*.mdx",
   },
-  theme: {
-    light: {
-      primary: "316 100% 50%",
-      primaryForeground: "360 100% 100%",
-    },
-    dark: {
-      primary: "316 100% 50%",
-      primaryForeground: "360 100% 100%",
-    },
-  },
+  // Authentication configuration
   authentication: {
     type: "auth0",
     domain: "ntotten-test.us.auth0.com",
     clientId: "s2YqBXJ0BDykcaIOEW4cXXacaUntA1bm",
   },
-  navigation,
-  redirects: [{ from: "/", to: "/documentation", replace: true }],
   apiKeys: {
     enabled: true,
     endpoint: "https://zudoku-customer-main-b36fa2f.d2.zuplo.dev",
-    // getConsumers: async () => {
-    //   return [
-    //     {
-    //       name: "consumer1",
-    //       apiKeys: [
-    //         {
-    //           key: "key_123",
-    //           id: "id_123",
-    //           createdOn: "2023-10-10",
-    //         },
-    //       ],
-    //     },
-    //   ];
-    // },
   },
   apis: {
     type: "url",
-    input: `https://blue-sloth-main-afc3428.d2.zuplo.dev/schemas/rewiring-america`,
-    path: "/api",
-    // server: "/__z/graphq",
-  },
-  docs: {
-    files: "/pages/**/*.mdx",
+    input: "https://rickandmorty.zuplo.io/openapi.json",
+    navigationId: "api",
   },
 };
 
