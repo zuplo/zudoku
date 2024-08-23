3ad35b67054f919e41fedd7f957b0672d7f0721e
Daniel Lehr <daniellehr@gmx.de>
2024-08-23 14:09:04 +0200

    Add playground test button

--- packages/zudoku/src/lib/plugins/openapi/playground/PlaygroundDialog.tsx
+++ packages/zudoku/src/lib/plugins/openapi/playground/PlaygroundDialog.tsx
@@ -1,5 +1,4 @@
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
-import { CirclePlayIcon } from "lucide-react";
 import { type PropsWithChildren, useState } from "react";
 import {
   Dialog,
@@ -11,16 +10,39 @@ import { Playground, type PlaygroundContentProps } from "./Playground.js";
 
 export type PlaygroundDialogProps = PropsWithChildren<PlaygroundContentProps>;
 
+const HeroPlayIcon = ({
+  className,
+  size = 16,
+}: {
+  className?: string;
+  size?: number;
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className={className}
+    width={size}
+    height={size}
+  >
+    <path
+      fillRule="evenodd"
+      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
 const PlaygroundDialog = (props: PlaygroundDialogProps) => {
   const [open, setOpen] = useState(false);
   return (
     <Dialog onOpenChange={(open) => setOpen(open)}>
       <DialogTrigger asChild>
         {props.children ?? (
-          <CirclePlayIcon
-            className="cursor-pointer text-primary hover:text-primary/80"
-            size={16}
-          />
+          <button className="flex gap-1 items-center px-2 py-1 rounded-md bg-primary/80 hover:bg-primary transition text-primary-foreground text-xs">
+            Test
+            <HeroPlayIcon className="" size={14} />
+          </button>
         )}
       </DialogTrigger>
 
