e738e35da715bc0b2d3f487aac136ed09fc601e6
Dan <dan@zuplo.com>
2024-08-30 11:08:48 +0200

    Fix sidebar menu height (#64)

--- packages/zudoku/src/lib/components/navigation/SidebarWrapper.tsx
+++ packages/zudoku/src/lib/components/navigation/SidebarWrapper.tsx
@@ -14,7 +14,7 @@ export const SidebarWrapper = forwardRef<
     className={cn(
       "scrollbar peer hidden lg:flex flex-col fixed text-sm overflow-y-auto shrink-0",
       "px-[--padding-nav-item] -mx-[--padding-nav-item] pb-20 mt-[--padding-content-top]",
-      "w-[--side-nav-width] h-full scroll-pt-2 gap-2",
+      "w-[--side-nav-width] h-[calc(100%-var(--header-height))] scroll-pt-2 gap-2",
       className,
     )}
     ref={ref}
