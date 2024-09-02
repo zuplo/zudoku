22708453c7aa5bac18d35531039732d3f743313e
Moritz <moritz@schwrr.com>
2024-09-02 19:15:51 +0200

    fine tuning

--- packages/zudoku/src/lib/demo/DemoAnnouncement.tsx
+++ packages/zudoku/src/lib/demo/DemoAnnouncement.tsx
@@ -1,7 +1,10 @@
 export const DemoAnnouncement = () => (
-  <div className="h-12 w-full bg-pink-700 text-center text-sm font-medium py-2">
+  <div className="h-12 w-full bg-pink-900 text-pink-100 text-center text-sm font-medium py-2 flex items-center justify-center ">
     This demo hosting of your OpenAPI isn't as fast or flexible as self-hosting;{" "}
-    <a href="https://github.com/zuplo/zudoku" className="">
+    <a
+      href="https://github.com/zuplo/zudoku"
+      className="px-1 underline hover:white"
+    >
       Get started here
     </a>{" "}
     to see Zudoku at full tilt.
