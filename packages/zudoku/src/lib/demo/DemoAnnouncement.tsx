91936d885ba8f79985f194a94907664960c2470d
Moritz <moritz@schwrr.com>
2024-09-02 14:41:39 +0200

    add component

---
+++ packages/zudoku/src/lib/demo/DemoAnnouncement.tsx
@@ -0,0 +1,11 @@
+export const DemoAnnouncement = () => (
+  <div className="h-12 w-full bg-pink-700 text-center text-sm font-medium py-2">
+    This demo hosting of your OpenAPI isn't as fast or flexible as self-hosting;{" "}
+    <a href="https://github.com/zuplo/zudoku" className="">
+      Get started here
+    </a>{" "}
+    to see Zudoku at full tilt.
+  </div>
+);
+
+export default DemoAnnouncement;
