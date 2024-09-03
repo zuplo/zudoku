b2a4dbfc04c529671441fbc35cb79372b926088a
Nathan Totten <nathan@ntotten.com>
2024-09-03 11:28:24 -0400

    Added docs link to header

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -16,10 +16,6 @@ const nextConfig = {
         destination: "https://cdn.zudoku.dev/demo",
       },
       {
-        source: "/introduction",
-        destination: "https://docs.zudoku.dev/introduction",
-      },
-      {
         source: "/docs/:path*",
         destination: "https://docs.zudoku.dev/docs/:path*",
       },
