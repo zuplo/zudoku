5b5b5d9404af7162f8b18ca028447611aa24ae27
Daniel Lehr <daniellehr@gmx.de>
2024-09-03 23:02:03 +0200

    Add mobile top navigation and sidebar

--- packages/zudoku/src/lib/components/Layout.tsx
+++ packages/zudoku/src/lib/components/Layout.tsx
@@ -1,6 +1,8 @@
 import { Helmet } from "@zudoku/react-helmet-async";
+import { PanelLeftIcon } from "lucide-react";
 import { Suspense, useEffect, useRef, type ReactNode } from "react";
 import { Outlet, useLocation } from "react-router-dom";
+import { Drawer, DrawerTrigger } from "../ui/Drawer.js";
 import { cn } from "../util/cn.js";
 import { useScrollToAnchor } from "../util/useScrollToAnchor.js";
 import { useScrollToTop } from "../util/useScrollToTop.js";
@@ -56,20 +58,33 @@ export const Layout = ({ children }: { children?: ReactNode }) => {
             </main>
           }
         >
-          <Sidebar />
-          <main
-            className={cn(
-              "h-full dark:border-white/10 translate-x-0",
-              "lg:overflow-visible",
-              // This works in tandem with the `SidebarWrapper` component
-              "lg:peer-data-[navigation=true]:w-[calc(100%-var(--side-nav-width))]",
-              "lg:peer-data-[navigation=true]:translate-x-[--side-nav-width] lg:peer-data-[navigation=true]:pl-12",
-            )}
-          >
-            <Slotlet name="zudoku-before-content" />
-            {children ?? <Outlet />}
-            <Slotlet name="zudoku-after-content" />
-          </main>
+          <Drawer direction="left">
+            <Sidebar />
+            <div
+              className={cn(
+                "lg:hidden -mx-10 px-10 py-2 sticky bg-background/80 backdrop-blur z-10 top-0 left-0 right-0 border-b",
+                "peer-data-[navigation=false]:hidden",
+              )}
+            >
+              <DrawerTrigger className="flex items-center gap-2">
+                <PanelLeftIcon size={16} strokeWidth={1.5} />
+                <span className="text-sm">Menu</span>
+              </DrawerTrigger>
+            </div>
+            <main
+              className={cn(
+                "h-full dark:border-white/10 translate-x-0",
+                "lg:overflow-visible",
+                // This works in tandem with the `SidebarWrapper` component
+                "lg:peer-data-[navigation=true]:w-[calc(100%-var(--side-nav-width))]",
+                "lg:peer-data-[navigation=true]:translate-x-[--side-nav-width] lg:peer-data-[navigation=true]:pl-12",
+              )}
+            >
+              <Slotlet name="zudoku-before-content" />
+              {children ?? <Outlet />}
+              <Slotlet name="zudoku-after-content" />
+            </main>
+          </Drawer>
         </Suspense>
       </div>
     </>
---
+++ packages/zudoku/src/lib/components/MobileTopNavigation.tsx
@@ -0,0 +1,52 @@
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { cx } from "class-variance-authority";
+import { MenuIcon } from "lucide-react";
+import { NavLink } from "react-router-dom";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerTitle,
+  DrawerTrigger,
+} from "../ui/Drawer.js";
+import { useZudoku } from "./context/ZudokuContext.js";
+
+export const MobileTopNavigation = () => {
+  const { topNavigation } = useZudoku();
+  return (
+    <Drawer direction="right">
+      <div className="flex lg:hidden justify-self-end">
+        <DrawerTrigger className="lg:hidden">
+          <MenuIcon size={22} />
+        </DrawerTrigger>
+      </div>
+      <DrawerContent
+        className="lg:hidden h-screen right-0 left-auto w-[320px] rounded-none"
+        aria-describedby={undefined}
+      >
+        <VisuallyHidden>
+          <DrawerTitle>Navigation</DrawerTitle>
+        </VisuallyHidden>
+        <ul className="flex flex-col items-center gap-4 p-4">
+          {topNavigation.map((item) => (
+            <li key={item.label}>
+              <NavLink
+                className={({ isActive }) =>
+                  cx(
+                    "block font-medium border-b-2",
+                    isActive
+                      ? "border-primary text-foreground"
+                      : "border-transparent text-foreground/75 hover:text-foreground hover:border-accent-foreground/25",
+                  )
+                }
+                to={item.id}
+              >
+                <DrawerClose>{item.label}</DrawerClose>
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </DrawerContent>
+    </Drawer>
+  );
+};
