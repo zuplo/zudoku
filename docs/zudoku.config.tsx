4272129e2803e9d2d910b5cd209e02cf44c30214
Daniel Lehr <daniellehr@gmx.de>
2024-09-05 19:54:36 +0200

    Add category icons

--- docs/zudoku.config.tsx
+++ docs/zudoku.config.tsx
@@ -31,11 +31,13 @@ const config: ZudokuConfig = {
       {
         type: "category",
         label: "Getting started",
+        icon: "sparkles",
         items: ["introduction", "app-quickstart", "html-quickstart"],
       },
       {
         type: "category",
         label: "Configuration",
+        icon: "cog",
         link: "configuration/overview",
         items: [
           "configuration/api-reference",
@@ -48,17 +50,20 @@ const config: ZudokuConfig = {
       {
         type: "category",
         label: "Markdown",
+        icon: "book-open-text",
         link: "markdown/overview",
         items: ["markdown/mdx", "markdown/admonitions", "markdown/code-blocks"],
       },
       {
         type: "category",
         label: "Guide",
+        icon: "monitor-check",
         items: ["environment-variables", "custom-pages", "using-multiple-apis"],
       },
       {
         type: "category",
         label: "Deployment",
+        icon: "cloud-upload",
         link: "deployment",
         items: [
           "deploy/cloudflare-pages",
@@ -70,6 +75,7 @@ const config: ZudokuConfig = {
       {
         type: "category",
         label: "Extending",
+        icon: "blocks",
         items: ["custom-plugins", "api-keys"],
       },
     ],
