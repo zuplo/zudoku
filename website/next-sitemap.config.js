5f5517ab8d3d1ca32718e72b01e6d4b5e06ab98f
Nathan Totten <nathan@ntotten.com>
2024-09-06 09:14:50 -0400

    updated next sitemap config

--- website/next-sitemap.config.js
+++ website/next-sitemap.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   siteUrl: "https://zudoku.dev",
   generateRobotsTxt: true,
-  exclude: ["/sitemap-*.xml"],
+  exclude: ["/sitemap-*.xml", "/icon.svg"],
+  additionalPaths: ["/"],
   robotsTxtOptions: {
     additionalSitemaps: ["https://zudoku.dev/docs/sitemap.xml"],
   },
