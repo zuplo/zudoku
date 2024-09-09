04d0a62949d3ce44277b7c108aac80592d50df00
Nathan Totten <nathan@ntotten.com>
2024-09-09 13:18:27 -0400

    expose plugin types

--- packages/zudoku/src/index.ts
+++ packages/zudoku/src/index.ts
@@ -2,3 +2,19 @@
 export type { ZudokuConfig } from "./config/config.js";
 export type { InputSidebar as Sidebar } from "./config/validators/InputSidebarSchema.js";
 export type { MDXImport } from "./lib/plugins/markdown/index.js";
+
+export {
+  DevPortalContext,
+  type ApiIdentity,
+} from "./lib/core/DevPortalContext.js";
+export type {
+  ApiIdentityPlugin,
+  CommonPlugin,
+  DevPortalPlugin,
+  NavigationPlugin,
+  ProfileMenuPlugin,
+  ProfileNavigationItem,
+  RouteObject,
+  SearchProviderPlugin,
+} from "./lib/core/plugins.js";
+export type { MdxComponentsType } from "./lib/util/MdxComponents.js";
