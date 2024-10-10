51f3f1a57b90462e28a99250bb5037150f98e638
Daniel Lehr <daniellehr@gmx.de>
2024-10-10 13:48:12 +0200

    Export sidebar entry

--- packages/zudoku/src/index.ts
+++ packages/zudoku/src/index.ts
@@ -1,5 +1,8 @@
 export type { ZudokuConfig } from "./config/config.js";
-export type { ConfigSidebar as Sidebar } from "./config/validators/InputSidebarSchema.js";
+export type {
+  SidebarConfig as Sidebar,
+  SidebarEntry,
+} from "./config/validators/InputSidebarSchema.js";
 export type { ExposedComponentProps } from "./lib/components/SlotletProvider.js";
 export type { MDXImport } from "./lib/plugins/markdown/index.js";
 
