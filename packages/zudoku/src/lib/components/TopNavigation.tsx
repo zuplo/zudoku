e8abed10ba3fb0c527045ac9ca8926fc53bf7c20
Moritz <moritz@schwrr.com>
2024-10-24 11:26:12 +0200

    Allow to show navigation only for auth/anon/all users

--- packages/zudoku/src/lib/components/TopNavigation.tsx
+++ packages/zudoku/src/lib/components/TopNavigation.tsx
@@ -1,10 +1,12 @@
 import { cx } from "class-variance-authority";
 import { NavLink } from "react-router-dom";
 
+import { useAuth } from "../authentication/hook.js";
 import { useZudoku } from "./context/ZudokuContext.js";
 
 export const TopNavigation = () => {
   const { topNavigation } = useZudoku();
+  const { isAuthenticated } = useAuth();
 
   // Hide top nav if there is only one item
   if (topNavigation.length <= 1) {
@@ -14,23 +16,31 @@ export const TopNavigation = () => {
   return (
     <nav className="hidden lg:block border-b text-sm px-12 h-[--top-nav-height]">
       <ul className="flex flex-row items-center gap-8">
-        {topNavigation.map((item) => (
-          <li key={item.label}>
-            <NavLink
-              className={({ isActive }) =>
-                cx(
-                  "block py-3.5 font-medium -mb-px border-b-2",
-                  isActive
-                    ? "border-primary text-foreground"
-                    : "border-transparent text-foreground/75 hover:text-foreground hover:border-accent-foreground/25",
-                )
-              }
-              to={item.id}
-            >
-              {item.label}
-            </NavLink>
-          </li>
-        ))}
+        {topNavigation
+          .filter(
+            (item) =>
+              (item.display === "auth" && isAuthenticated) ||
+              (item.display === "anon" && !isAuthenticated) ||
+              !item.display ||
+              item.display === "always",
+          )
+          .map((item) => (
+            <li key={item.label}>
+              <NavLink
+                className={({ isActive }) =>
+                  cx(
+                    "block py-3.5 font-medium -mb-px border-b-2",
+                    isActive
+                      ? "border-primary text-foreground"
+                      : "border-transparent text-foreground/75 hover:text-foreground hover:border-accent-foreground/25",
+                  )
+                }
+                to={item.id}
+              >
+                {item.label}
+              </NavLink>
+            </li>
+          ))}
       </ul>
     </nav>
   );
