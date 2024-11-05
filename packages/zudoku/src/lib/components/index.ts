9e716bf4407c1294e11f439c5659661be67db24a
Moritz <moritz@schwrr.com>
2024-11-05 12:44:16 +0100

    expose auth for external to use (#317)
    * expose auth for external to use
    
    * Update packages/zudoku/src/lib/components/index.ts
    
    * add client only component
    
    ---------
    
    Co-authored-by: Dan <dan@zuplo.com>

--- packages/zudoku/src/lib/components/index.ts
+++ packages/zudoku/src/lib/components/index.ts
@@ -1,6 +1,7 @@
 import { useMDXComponents as useMDXComponentsImport } from "@mdx-js/react";
 import { Helmet } from "@zudoku/react-helmet-async";
 import { Link as LinkImport } from "react-router-dom";
+import { useAuthState } from "../authentication/state.js";
 import { RouterError as RouterErrorImport } from "../errors/RouterError.js";
 import { ServerError as ServerErrorImport } from "../errors/ServerError.js";
 import { Button as ButtonImport } from "../ui/Button.js";
@@ -9,19 +10,25 @@ import {
   Bootstrap as BootstrapImport,
   BootstrapStatic as BootstrapStaticImport,
 } from "./Bootstrap.js";
+import { ClientOnly as ClientOnlyImport } from "./ClientOnly.js";
 import { DevPortal as DevPortalImport } from "./DevPortal.js";
 import { Layout as LayoutImport } from "./Layout.js";
 import { useZudoku as useZudokuImport } from "./context/ZudokuContext.js";
-
 export const useMDXComponents = /*@__PURE__*/ useMDXComponentsImport;
-export const Callout = /*@__PURE__*/ CalloutImport;
 export const DevPortal = /*@__PURE__*/ DevPortalImport;
 export const Layout = /*@__PURE__*/ LayoutImport;
-export const Link: typeof LinkImport = /*@__PURE__*/ LinkImport;
 export const RouterError = /*@__PURE__*/ RouterErrorImport;
 export const ServerError = /*@__PURE__*/ ServerErrorImport;
 export const Bootstrap = /*@__PURE__*/ BootstrapImport;
 export const BootstrapStatic = /*@__PURE__*/ BootstrapStaticImport;
-export const Button = /*@__PURE__*/ ButtonImport;
+
 export const Head = /*@__PURE__*/ Helmet;
+
 export const useZudoku = /*@__PURE__*/ useZudokuImport;
+export const useAuth = /*@__PURE__*/ useAuthState;
+export const Zudoku = /*@__PURE__*/ DevPortalImport;
+
+export const Callout = /*@__PURE__*/ CalloutImport;
+export const ClientOnly = /*@__PURE__*/ ClientOnlyImport;
+export const Button = /*@__PURE__*/ ButtonImport;
+export const Link: typeof LinkImport = /*@__PURE__*/ LinkImport;
