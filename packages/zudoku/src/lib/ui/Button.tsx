8d7520fb1f4724b13cba28756eca0ad422e2906e
Daniel Lehr <daniellehr@gmx.de>
2024-11-08 10:01:06 +0100

    Add common UI components

--- packages/zudoku/src/lib/ui/Button.tsx
+++ packages/zudoku/src/lib/ui/Button.tsx
@@ -3,7 +3,7 @@ import { cva, VariantProps } from "class-variance-authority";
 import * as React from "react";
 import { cn } from "../util/cn.js";
 
-const buttonVariants = cva(
+export const buttonVariants = cva(
   "not-prose inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
