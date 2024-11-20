2c8c71e30ae84c2bd66cf49b8e141884ef5205a0
Daniel Lehr <daniellehr@gmx.de>
2024-11-20 13:10:45 +0100

    Add ActionButton component

---
+++ packages/zudoku/src/lib/ui/ActionButton.tsx
@@ -0,0 +1,28 @@
+import { forwardRef } from "react";
+import { Button, type ButtonProps } from "zudoku/ui/Button.js";
+import { Spinner } from "../components/Spinner.js";
+import { cn } from "../util/cn.js";
+
+type ActionButtonProps = ButtonProps & { isPending?: boolean };
+
+export const ActionButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ isPending, children, className, ...props }: ActionButtonProps, ref) => {
+    return (
+      <Button
+        ref={ref}
+        disabled={isPending}
+        {...props}
+        className={cn("relative", className)}
+      >
+        {isPending && (
+          <div className="absolute inset-0 grid place-items-center">
+            <Spinner />
+          </div>
+        )}
+        <div className={cn(isPending && "invisible")}>{children}</div>
+      </Button>
+    );
+  },
+);
+
+ActionButton.displayName = "ActionButton";
