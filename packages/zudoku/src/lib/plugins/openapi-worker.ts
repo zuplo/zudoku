d33441449e91e360d70a4c69fb3b69f827fdbef7
Daniel Lehr <daniellehr@gmx.de>
2024-11-25 20:04:39 +0100

    fix worker communication

--- packages/zudoku/src/lib/plugins/openapi-worker.ts
+++ packages/zudoku/src/lib/plugins/openapi-worker.ts
@@ -1,1 +1,11 @@
-export { GraphQLClient } from "./openapi/client/GraphQLClient.js";
+export const initializeWorker = () => {
+  const worker = new SharedWorker(
+    new URL("./openapi/client/worker.ts", import.meta.url),
+    { type: "module" },
+  );
+  // eslint-disable-next-line no-console
+  worker.onerror = (e) => console.error(e);
+  worker.port.start();
+
+  return worker;
+};
