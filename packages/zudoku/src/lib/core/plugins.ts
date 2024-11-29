a04c4fc87fd654a8b8c19627816a5f7e19484aed
Moritz <moritz@schwrr.com>
2024-11-29 15:28:58 +0100

    type?

--- packages/zudoku/src/lib/core/plugins.ts
+++ packages/zudoku/src/lib/core/plugins.ts
@@ -39,7 +39,10 @@ export type ProfileNavigationItem = {
   weight?: number;
   category?: "top" | "middle" | "bottom";
   children?: ProfileNavigationItem[];
-  icon?: React.ComponentType;
+  icon?: React.ComponentType<{
+    size: number;
+    [key: string]: any;
+  }>;
 };
 
 export interface CommonPlugin {
