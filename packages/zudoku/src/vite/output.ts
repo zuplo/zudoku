0ab53d4dc8b2c05e21811dcbd80449911551bea7
Dan <dan@zuplo.com>
2024-12-12 19:48:16 +0100

    Remove redirect

--- packages/zudoku/src/vite/output.ts
+++ packages/zudoku/src/vite/output.ts
@@ -141,15 +141,6 @@ type CronsConfig = Cron[];
 export function generateOutput(config: LoadedConfig): Config {
   const routes: Route[] = [];
 
-  if (config.basePath) {
-    routes.push({
-      src: "^/$",
-      dest: config.basePath,
-      status: 301,
-      headers: { Location: config.basePath },
-    });
-  }
-
   if (config.redirects) {
     for (const redirect of config.redirects) {
       routes.push({
