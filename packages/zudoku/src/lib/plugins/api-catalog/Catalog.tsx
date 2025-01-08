9efd48a71a1a6b498647d191396aa1657adc021e
Moritz <moritz@schwrr.com>
2025-01-08 10:53:22 +0100

    Feat/category enhancements (#443)
    * Catalog enhancements
    
    * Add filter
    
    * add generic
    
    * pass generic
    
    * add to sidebar
    
    * allow to filter items
    
    * better typing
    
    * Fix plugin code generation
    
    * Exclude from standalone build
    
    ---------
    
    Co-authored-by: Daniel Lehr <daniellehr@gmx.de>

--- packages/zudoku/src/lib/plugins/api-catalog/Catalog.tsx
+++ packages/zudoku/src/lib/plugins/api-catalog/Catalog.tsx
@@ -1,37 +1,45 @@
 import slugify from "@sindresorhus/slugify";
-import { Fragment } from "react";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
 import { Head, Link } from "zudoku/components";
+import { useAuthState } from "../../authentication/state.js";
 import { Markdown } from "../../components/Markdown.js";
-import { useExposedProps } from "../../util/useExposedProps.js";
-import type { ApiCatalogItem, CatalogCategory } from "./index.js";
+import { cn } from "../../util/cn.js";
+import type { ApiCatalogPluginOptions } from "./index.js";
+
+const getKey = (category: string, tag: string) => slugify(`${category}-${tag}`);
 
 export const Catalog = ({
   items,
+  filterCatalogItems = (items) => items,
   categories,
   label = "API Library",
-}: {
-  label: string;
-  items: ApiCatalogItem[];
-  categories: CatalogCategory[];
-}) => {
-  const { searchParams, setSearchParams } = useExposedProps();
+}: Omit<ApiCatalogPluginOptions, "navigationId">) => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const activeCategory = searchParams.get("category");
+  const auth = useAuthState();
+
+  const catalogItems = useSuspenseQuery({
+    queryFn: () => filterCatalogItems(items, { auth }),
+    queryKey: ["catalogItems", auth],
+  });
+
   return (
     <section className="pt-[--padding-content-top] pb-[--padding-content-bottom]">
       <Head>
         <title>{label}</title>
       </Head>
       <div className="grid grid-cols-12 gap-12">
-        <div className="flex flex-col gap-4 col-span-3 px-4 not-prose sticky top-48">
-          <div className="justify-between">
-            {categories.map((category, idx) => (
-              <Fragment key={category.label}>
+        <div className="flex flex-col gap-4 col-span-3 not-prose sticky top-48">
+          <div className="max-w-[--side-nav-width] flex flex-col gap-4 justify-between">
+            {categories?.map((category, idx) => (
+              <div key={category.label}>
                 <div className="flex justify-between mb-2.5">
                   <span className="font-medium text-sm">{category.label}</span>
                   {idx === 0 && activeCategory && (
                     <button
                       type="button"
-                      className="text-end text-sm mr-8 text-foreground/60 hover:text-foreground"
+                      className="text-end text-sm text-foreground/60 hover:text-foreground"
                       onClick={() => setSearchParams({})}
                     >
                       Clear
@@ -39,45 +47,41 @@ export const Catalog = ({
                   )}
                 </div>
                 <ul className="space-y-1 [&>li]:py-2">
-                  {Array.from(
-                    new Set(
-                      category.tags
-                        .map((tag) => ({
-                          tag,
-                          count: items.filter((api) =>
-                            api.categories.find((c) => c.tags.includes(tag)),
-                          ).length,
-                        }))
-                        .map(({ tag, count }) => (
-                          <li
-                            key={slugify(category.label + " " + tag)}
-                            className={`flex px-4 rounded-lg -translate-x-4 justify-between text-sm cursor-pointer hover:text-primary transition ${
-                              slugify(tag) === activeCategory
-                                ? "font-medium bg-border/30 rounded"
-                                : ""
-                            }`}
-                            onClick={() =>
-                              setSearchParams({
-                                category: slugify(category.label + " " + tag),
-                              })
-                            }
+                  {category.tags
+                    .map((tag) => ({
+                      tag,
+                      count: items.filter((api) =>
+                        api.categories.find((c) => c.tags.includes(tag)),
+                      ).length,
+                    }))
+                    .map(({ tag, count }) => {
+                      const slug = getKey(category.label, tag);
+                      const isActive = slug === activeCategory;
+
+                      return (
+                        <li
+                          key={slug}
+                          className={cn(
+                            "flex rounded-lg justify-between text-sm cursor-pointer hover:text-primary transition px-[--padding-nav-item] -mx-[--padding-nav-item]",
+                            isActive && "bg-border/30 rounded",
+                          )}
+                          onClick={() => setSearchParams({ category: slug })}
+                        >
+                          <span>{tag}</span>
+                          <span
+                            className={cn(
+                              "flex items-center justify-center border rounded-md w-8 text-xs font-semibold",
+                              isActive &&
+                                "bg-primary border-primary text-primary-foreground",
+                            )}
                           >
-                            <span>{tag}</span>
-                            <span
-                              className={`flex items-center justify-center border rounded-md w-8 text-xs font-semibold ${
-                                slugify(tag) === activeCategory
-                                  ? "bg-primary border-primary text-primary-foreground"
-                                  : ""
-                              }`}
-                            >
-                              {count}
-                            </span>
-                          </li>
-                        )),
-                    ),
-                  )}
+                            {count}
+                          </span>
+                        </li>
+                      );
+                    })}
                 </ul>
-              </Fragment>
+              </div>
             ))}
           </div>
         </div>
@@ -85,17 +89,15 @@ export const Catalog = ({
           <h3 className="mt-0 text-2xl font-bold mb-4">{label}</h3>
 
           <div className="grid grid-cols-2 gap-4">
-            {items
+            {catalogItems.data
               .filter(
                 (api) =>
                   !activeCategory ||
                   api.categories.find((c) =>
-                    c.tags.find(
-                      (t) => slugify(c.label + " " + t) === activeCategory,
-                    ),
+                    c.tags.find((t) => getKey(c.label, t) === activeCategory),
                   ),
               )
-              .map((api, i) => (
+              .map((api) => (
                 <Link
                   to={{
                     pathname: `/${api.path}`,
@@ -104,10 +106,7 @@ export const Catalog = ({
                   className="no-underline hover:!text-foreground"
                   key={api.path}
                 >
-                  <div
-                    className="border h-full rounded p-4 flex flex-col gap-2 cursor-pointer hover:bg-border/20 font-normal"
-                    key={i}
-                  >
+                  <div className="border h-full rounded p-4 flex flex-col gap-2 cursor-pointer hover:bg-border/20 font-normal">
                     <span className="font-semibold">{api.label}</span>
                     <Markdown
                       className="text-sm whitespace-pre-wrap mb-6 line-clamp-2"
