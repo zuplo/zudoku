fabd0c1e6986cbf8d8663bf640d1490b1ea2b26d
Moritz <moritz@schwrr.com>
2025-01-09 11:10:31 +0100

    Show full URL for method (#176)
    * Show full URL for method
    
    * select text and truncate

--- packages/zudoku/src/lib/plugins/openapi/OperationListItem.tsx
+++ packages/zudoku/src/lib/plugins/openapi/OperationListItem.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Heading } from "../../components/Heading.js";
 import { Markdown, ProseClasses } from "../../components/Markdown.js";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../ui/Tabs.js";
@@ -9,20 +9,24 @@ import { ParameterList } from "./ParameterList.js";
 import { Sidecar } from "./Sidecar.js";
 import { FragmentType, useFragment } from "./graphql/index.js";
 import { SchemaView } from "./schema/SchemaView.js";
+import { methodForColor } from "./util/methodToColor.js";
 
 export const PARAM_GROUPS = ["path", "query", "header", "cookie"] as const;
 export type ParameterGroup = (typeof PARAM_GROUPS)[number];
 
 export const OperationListItem = ({
   operationFragment,
+  serverUrl,
 }: {
   operationFragment: FragmentType<typeof OperationsFragment>;
+  serverUrl?: string;
 }) => {
   const operation = useFragment(OperationsFragment, operationFragment);
   const groupedParameters = groupBy(
     operation.parameters ?? [],
     (param) => param.in,
   );
+  const parentRef = useRef<HTMLDivElement>(null);
 
   const first = operation.responses.at(0);
   const [selectedResponse, setSelectedResponse] = useState(first?.statusCode);
@@ -30,12 +34,37 @@ export const OperationListItem = ({
   return (
     <div
       key={operation.operationId}
-      className="grid grid-cols-1 lg:grid-cols-[4fr_3fr] gap-8 items-start border-b-2 mb-16 pb-16"
+      className="grid grid-cols-1 lg:grid-cols-[minmax(0,4fr)_minmax(0,3fr)] gap-8 items-start border-b-2 mb-16 pb-16"
     >
       <div className="flex flex-col gap-4">
         <Heading level={2} id={operation.slug} registerSidebarAnchor>
           {operation.summary}
         </Heading>
+        <div className="text-sm flex gap-2 font-mono">
+          <span className={methodForColor(operation.method)}>
+            {operation.method.toUpperCase()}
+          </span>
+          <div
+            ref={parentRef}
+            className="max-w-full truncate flex cursor-pointer"
+            onClick={() => {
+              if (parentRef.current) {
+                const range = document.createRange();
+                range.selectNodeContents(parentRef.current);
+                const selection = window.getSelection();
+                selection?.removeAllRanges();
+                selection?.addRange(range);
+              }
+            }}
+          >
+            <div className="text-neutral-400 dark:text-neutral-500 truncate">
+              {serverUrl}
+            </div>
+            <div className="text-neutral-900 dark:text-neutral-200">
+              {operation.path}
+            </div>
+          </div>
+        </div>
         {operation.description && (
           <Markdown
             className={`${ProseClasses} max-w-full prose-img:max-w-prose`}
