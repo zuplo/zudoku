6ff1538a62f024bb79e3273c1e3ff798ff93a70a
Moritz <moritz@schwrr.com>
2025-01-19 19:12:55 +0100

    feat(authentication): implement user state management in Clerk provider

--- packages/zudoku/src/lib/authentication/components/SignOut.tsx
+++ packages/zudoku/src/lib/authentication/components/SignOut.tsx
@@ -1,13 +1,18 @@
-import { useEffect } from "react";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { useNavigate } from "react-router";
 import { useZudoku } from "../../components/context/ZudokuContext.js";
 
 export const SignOut = () => {
   const context = useZudoku();
   const navigate = useNavigate();
-  useEffect(() => {
-    void context.authentication?.signOut().then(() => navigate("/"));
-  }, [navigate, context.authentication]);
+
+  useSuspenseQuery({
+    queryKey: ["signout"],
+    queryFn: async () => {
+      void (await context.authentication?.signOut());
+      void navigate("/");
+    },
+  });
 
   return null;
 };
