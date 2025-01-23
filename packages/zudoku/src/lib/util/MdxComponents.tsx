f4834fc0abbfc341d130516d856eb1e0f7639b82
Moritz <moritz@schwrr.com>
2025-01-23 20:18:24 +0100

    fix type

--- packages/zudoku/src/lib/util/MdxComponents.tsx
+++ packages/zudoku/src/lib/util/MdxComponents.tsx
@@ -61,10 +61,7 @@ export const MdxComponents = {
   code: ({ className, children, ...props }) => {
     // `inline` provided by the rehype plugin, as react-markdown removed support for that
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const { inline, title } = props as Record<
-      string,
-      string | unknown | undefined
-    >;
+    const { inline, title } = props as Record<string, string | boolean>;
 
     if (inline === true || inline === "true") {
       return <InlineCode className={className}>{children}</InlineCode>;
@@ -78,7 +75,7 @@ export const MdxComponents = {
         className="rounded-xl p-4 border dark:!bg-foreground/10 dark:border-transparent"
         showLanguageIndicator
         code={String(children).trim()}
-        title={title}
+        title={String(title) ?? undefined}
       />
     );
   },
