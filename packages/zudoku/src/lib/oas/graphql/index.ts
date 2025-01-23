81f82e173998b46d6a060ac62653075c8aceb894
Moritz <moritz@schwrr.com>
2025-01-23 21:41:35 +0100

    remove

--- packages/zudoku/src/lib/oas/graphql/index.ts
+++ packages/zudoku/src/lib/oas/graphql/index.ts
@@ -450,7 +450,6 @@ builder.queryType({
       args: {
         type: t.arg({ type: SchemaSource, required: true }),
         input: t.arg({ type: JSONScalar, required: true }),
-        baseDir: t.arg({ type: "String", required: false }),
       },
       resolve: async (_, args, ctx) => {
         let schema: OpenAPIDocument;
@@ -464,10 +463,7 @@ builder.queryType({
           const module = await loadSchema();
           schema = module.schema;
         } else {
-          schema = await validate(
-            args.input as string,
-            args.baseDir ?? undefined,
-          );
+          schema = await validate(args.input as string);
         }
 
         ctx.schema = schema;
