60ba6524ff5e45e68cf23161853220f8aaa03bc1
Moritz <moritz@schwrr.com>
2025-01-24 12:15:11 +0100

    feat: add slotlets for GitHub and Discord links in header (#588)

--- packages/zudoku/src/lib/components/ThemeSwitch.tsx
+++ packages/zudoku/src/lib/components/ThemeSwitch.tsx
@@ -1,26 +1,25 @@
 import { MoonStarIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "zudoku/ui/Button.js";
-import { ClientOnly } from "./ClientOnly.js";
+import { cn } from "../util/cn.js";
 
 export const ThemeSwitch = () => {
   const { resolvedTheme, setTheme } = useTheme();
   const ThemeIcon = resolvedTheme === "dark" ? MoonStarIcon : SunIcon;
 
   return (
-    <ClientOnly>
-      <Button
-        variant="ghost"
-        aria-label={
-          resolvedTheme === "dark"
-            ? "Switch to light mode"
-            : "Switch to dark mode"
-        }
-        className="p-2.5 -m-2.5 rounded-full"
-        onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-      >
-        <ThemeIcon size={18} />
-      </Button>
-    </ClientOnly>
+    <Button
+      variant="ghost"
+      size="icon"
+      aria-label={
+        resolvedTheme === "dark"
+          ? "Switch to light mode"
+          : "Switch to dark mode"
+      }
+      className={cn(!resolvedTheme && "opacity-0")}
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+    >
+      <ThemeIcon size={18} />
+    </Button>
   );
 };
