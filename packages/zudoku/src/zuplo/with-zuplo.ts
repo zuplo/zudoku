6b7d0fe01bd2a47f1b648cc7d70dcfe17f6d6c90
Nathan Totten <nate@zuplo.com>
2025-01-27 11:08:22 -0500

    Change withZuplo to be default export (#605)

--- packages/zudoku/src/zuplo/with-zuplo.ts
+++ packages/zudoku/src/zuplo/with-zuplo.ts
@@ -1,11 +1,11 @@
 import { CommonConfig } from "../config/validators/common.js";
 
-export const withZuplo = <TConfig extends CommonConfig>(
-  config: TConfig,
-): TConfig => {
+const withZuplo = <TConfig extends CommonConfig>(config: TConfig): TConfig => {
   return {
     ...config,
     isZuplo: true,
     enableStatusPages: true,
   };
 };
+
+export default withZuplo;
