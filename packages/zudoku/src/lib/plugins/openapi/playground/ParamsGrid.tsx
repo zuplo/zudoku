7ab6d2c8896cb23fed676a5362ab4dedcbd93cea
Daniel Lehr <daniellehr@gmx.de>
2025-01-31 11:15:18 +0100

    Use `createVariantComponent` as well

--- packages/zudoku/src/lib/plugins/openapi/playground/ParamsGrid.tsx
+++ packages/zudoku/src/lib/plugins/openapi/playground/ParamsGrid.tsx
@@ -1,28 +1,13 @@
-import { type ReactNode } from "react";
-import { cn } from "../../../util/cn.js";
 import createVariantComponent from "../../../util/createVariantComponent.js";
+
 const ParamsGrid = createVariantComponent(
   "div",
   "grid grid-cols-[2fr_3fr] gap-2 items-center",
 );
 
-export const ParamsGridItem = ({
-  children,
-  className,
-}: {
-  children: ReactNode;
-  className?: string;
-}) => {
-  return (
-    <div
-      className={cn(
-        "group hover:bg-accent/40 px-3 grid col-span-full grid-cols-subgrid",
-        className,
-      )}
-    >
-      {children}
-    </div>
-  );
-};
+export const ParamsGridItem = createVariantComponent(
+  "div",
+  "group hover:bg-accent px-3 grid col-span-full grid-cols-subgrid",
+);
 
 export default ParamsGrid;
