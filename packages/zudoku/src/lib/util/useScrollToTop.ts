6eed6bff049dc353c1894a75737dd66555997bac
Daniel Lehr <daniellehr@gmx.de>
2025-02-05 15:31:09 +0100

    Fix scroll to element when router page changes

--- packages/zudoku/src/lib/util/useScrollToTop.ts
+++ packages/zudoku/src/lib/util/useScrollToTop.ts
@@ -4,10 +4,17 @@ import { useLocation } from "react-router";
 export const useScrollToTop = () => {
   const location = useLocation();
   const previousPath = useRef(location.pathname);
+  const previousHash = useRef(location.hash);
 
   useEffect(() => {
-    if (previousPath.current === location.pathname) return;
-    window.scrollTo(0, 0);
+    const isNewPage = previousPath.current !== location.pathname;
+    const hasAnchor = location.hash !== "";
+
+    if (isNewPage && !hasAnchor) {
+      window.scrollTo(0, 0);
+    }
+
     previousPath.current = location.pathname;
-  }, [location.pathname]);
+    previousHash.current = location.hash;
+  }, [location.pathname, location.hash]);
 };
