e43a825e0cca2e8559612a06caa3f1e3f4b88ff5
Moritz <moritz@schwrr.com>
2025-02-12 10:41:31 +0100

    details on theme switch (#666)

--- packages/zudoku/src/lib/components/ThemeSwitch.tsx
+++ packages/zudoku/src/lib/components/ThemeSwitch.tsx
@@ -1,6 +1,7 @@
-import { MoonStarIcon, SunIcon } from "lucide-react";
+import { MoonIcon, MoonStarIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "zudoku/ui/Button.js";
+import { cn } from "../util/cn.js";
 import { ClientOnly } from "./ClientOnly.js";
 
 export const ThemeSwitch = () => {
@@ -9,18 +10,37 @@ export const ThemeSwitch = () => {
 
   return (
     <ClientOnly fallback={<Button variant="ghost" size="icon" />}>
-      <Button
-        variant="ghost"
-        size="icon"
+      <button
+        type="button"
+        className="flex rounded-xl border p-0.5 gap-0.5 group"
+        onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
         aria-label={
           resolvedTheme === "dark"
             ? "Switch to light mode"
             : "Switch to dark mode"
         }
-        onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
       >
-        <ThemeIcon size={18} />
-      </Button>
+        <div
+          className={cn(
+            " rounded-full p-0.5 [&>svg>circle]:transition-colors [&>svg>circle]:duration-500 [&>svg>circle]:fill-transparent",
+            resolvedTheme === "light" && "border bg-muted/50",
+            resolvedTheme === "dark" &&
+              "group-hover:[&>svg>circle]:fill-primary",
+          )}
+        >
+          <SunIcon size={16} />
+        </div>
+        <div
+          className={cn(
+            "rounded-full p-0.5 transition-all transform duration-500 ",
+            resolvedTheme === "dark" &&
+              "border bg-card [&>svg>path]:fill-white",
+            resolvedTheme === "light" && "group-hover:rotate-[-10deg]",
+          )}
+        >
+          <MoonIcon size={16} />
+        </div>
+      </button>
     </ClientOnly>
   );
 };
