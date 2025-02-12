a2cda579ebc8329de6ead741ba3b9eafd976f654
Moritz <moritz@schwrr.com>
2025-02-12 11:38:03 +0100

    Better transition, animations & roundness (#667)

--- packages/zudoku/src/lib/components/ThemeSwitch.tsx
+++ packages/zudoku/src/lib/components/ThemeSwitch.tsx
@@ -12,7 +12,7 @@ export const ThemeSwitch = () => {
     <ClientOnly fallback={<Button variant="ghost" size="icon" />}>
       <button
         type="button"
-        className="flex rounded-xl border p-0.5 gap-0.5 group"
+        className="flex rounded-full border p-0.5 gap-0.5 group"
         onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
         aria-label={
           resolvedTheme === "dark"
@@ -22,20 +22,21 @@ export const ThemeSwitch = () => {
       >
         <div
           className={cn(
-            " rounded-full p-0.5 [&>svg>circle]:transition-colors [&>svg>circle]:duration-500 [&>svg>circle]:fill-transparent",
+            "rounded-full p-0.5 [&>svg>circle]:transition-colors [&>svg>path]:transition-transform transition-all [&>svg>path]:duration-200 [&>svg>circle]:duration-500 [&>svg>circle]:fill-transparent",
             resolvedTheme === "light" && "border bg-muted/50",
             resolvedTheme === "dark" &&
-              "group-hover:[&>svg>circle]:fill-primary",
+              "group-hover:[&>svg>path]:scale-110 group-hover:[&>svg>path]:-translate-x-[1px] group-hover:[&>svg>path]:-translate-y-[1px] group-hover:rotate-[15deg] ",
           )}
         >
           <SunIcon size={16} />
         </div>
         <div
           className={cn(
-            "rounded-full p-0.5 transition-all transform duration-500 ",
+            "rounded-full p-0.5 transition-transform transform-gpu duration-500",
             resolvedTheme === "dark" &&
-              "border bg-card [&>svg>path]:fill-white",
-            resolvedTheme === "light" && "group-hover:rotate-[-10deg]",
+              "border bg-muted [&>svg>path]:fill-white [&>svg>path]:stroke-transparent",
+            resolvedTheme === "light" &&
+              "group-hover:rotate-[-10deg] [&>svg>path]:stroke-currentColor",
           )}
         >
           <MoonIcon size={16} />
