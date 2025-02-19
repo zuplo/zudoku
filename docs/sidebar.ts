9a824f6928930bb97d257bbbd3e39e8b7cde723e
Moritz <moritz@schwrr.com>
2025-02-19 11:23:56 +0100

    add to sidebar

--- docs/sidebar.ts
+++ docs/sidebar.ts
@@ -69,7 +69,7 @@ export const docs: SidebarEntry = [
     type: "category",
     label: "Extending",
     icon: "blocks",
-    items: ["custom-plugins", "api-keys"],
+    items: ["custom-plugins", "extending/events"],
   },
 ];
 export const components: SidebarEntry = [
