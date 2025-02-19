36d836b21ce269eedfe47516b7323f5f64a6c76d
Daniel Lehr <daniellehr@gmx.de>
2025-02-19 17:45:35 +0100

    Extend useEvent and add tests

--- packages/zudoku/src/lib/hooks/useEvent.ts
+++ packages/zudoku/src/lib/hooks/useEvent.ts
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import { useZudoku } from "../components/context/ZudokuContext.js";
 import type { ZudokuEvents } from "../core/ZudokuContext.js";
 
+type EventParameters<Event extends keyof ZudokuEvents> = Parameters<
+  ZudokuEvents[Event]
+>;
+
 /**
  * Hook to subscribe to Zudoku events with automatic cleanup
  * @param event The event to subscribe to
@@ -10,23 +14,28 @@ import type { ZudokuEvents } from "../core/ZudokuContext.js";
  */
 export function useEvent<E extends keyof ZudokuEvents>(
   event: E,
-  callback?: ZudokuEvents[E],
+): EventParameters<E>;
+export function useEvent<E extends keyof ZudokuEvents, R>(
+  event: E,
+  callback: (...args: EventParameters<E>) => R,
+): R;
+export function useEvent<E extends keyof ZudokuEvents, R>(
+  event: E,
+  callback?: (...args: EventParameters<E>) => R,
 ) {
   const zudoku = useZudoku();
-  const [latestData, setLatestData] =
-    useState<Parameters<ZudokuEvents[E]>[0]>();
+  const [latestData, setLatestData] = useState<R | EventParameters<E>>();
 
   useEffect(() => {
-    const handler: ZudokuEvents[E] = ((...args) => {
-      const result = callback?.(...args);
-      if (!callback || result !== undefined) {
-        setLatestData(result ?? args[0]);
+    return zudoku.addEventListener(event, ((...args: EventParameters<E>) => {
+      if (callback) {
+        const result = callback(...args);
+        setLatestData(result);
+      } else {
+        setLatestData(args);
       }
-    }) as ZudokuEvents[E];
-
-    const unsubscribe = zudoku.addEventListener(event, handler);
-    return () => unsubscribe();
+    }) as ZudokuEvents[E]);
   }, [zudoku, event, callback]);
 
-  return callback ? undefined : latestData;
+  return latestData;
 }
