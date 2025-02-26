407d9fb417cd9a11588f29de2c6d4997ca939cd8
Daniel Lehr <daniellehr@gmx.de>
2025-02-26 16:42:08 +0100

    Style improvements

--- packages/zudoku/src/lib/plugins/search-pagefind/ResultList.tsx
+++ packages/zudoku/src/lib/plugins/search-pagefind/ResultList.tsx
@@ -1,3 +1,4 @@
+import { FileTextIcon } from "lucide-react";
 import { Link, useNavigate } from "react-router";
 import { CommandGroup, CommandItem, CommandList } from "zudoku/ui/Command.js";
 import { PagefindSearchFragment, PagefindSubResult } from "./types.js";
@@ -14,6 +15,8 @@ const sortSubResults = (a: PagefindSubResult, b: PagefindSubResult) => {
   return bScore - aScore;
 };
 
+const hoverClassname = `cursor-pointer border border-transparent data-[selected=true]:border-border`;
+
 export const ResultList = ({
   searchResults,
   onClose,
@@ -27,12 +30,21 @@ export const ResultList = ({
     <CommandList className="max-h-[450px]">
       {searchResults.map((result) => (
         <CommandGroup
-          heading={result.meta.title ?? ""}
           key={[result.meta.title ?? result.excerpt, result.url].join("-")}
         >
+          <CommandItem
+            asChild
+            value={`${result.meta.title}-${result.url}`}
+            className={hoverClassname}
+          >
+            <Link to={result.url}>
+              <FileTextIcon size={20} className="text-muted-foreground" />
+              {result.meta.title}
+            </Link>
+          </CommandItem>
           {result.sub_results
             .sort(sortSubResults)
-            .slice(0, 3)
+            .slice(0, 4)
             .map((subResult) => {
               const url = subResult.url.replace(".html", "");
               const navigateTo = url.startsWith(import.meta.env.BASE_URL)
@@ -41,23 +53,22 @@ export const ResultList = ({
 
               return (
                 <CommandItem
+                  asChild
                   key={`${result.meta.title}-${subResult.url}-${subResult.excerpt}`}
-                  className="flex flex-col items-start"
+                  className={hoverClassname}
                   onSelect={() => {
                     void navigate(navigateTo);
                     onClose();
                   }}
                 >
-                  <span className="font-bold">{subResult.title}</span>
-                  <span
-                    className="text-xs"
-                    dangerouslySetInnerHTML={{ __html: subResult.excerpt }}
-                  />
-                  <Link
-                    className="text-xs text-muted-foreground"
-                    to={navigateTo}
-                  >
-                    {navigateTo}
+                  <Link to={navigateTo} onClick={onClose}>
+                    <div className="flex flex-col items-start gap-2 border-l border-muted-foreground/50 ms-2.5 ps-5">
+                      <span className="font-bold">{subResult.title}</span>
+                      <span
+                        className="text-xs [&_mark]:bg-primary/75 [&_mark]:p-0.5 [&_mark]:border-primary [&_mark]:text-primary-foreground"
+                        dangerouslySetInnerHTML={{ __html: subResult.excerpt }}
+                      />
+                    </div>
                   </Link>
                 </CommandItem>
               );
