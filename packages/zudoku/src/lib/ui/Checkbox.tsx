e6b21fad35e239ddf1de6d3b9e7c88193e961a77
Moritz <moritz@schwrr.com>
2025-02-28 16:22:12 +0100

    make checkbox not round

--- packages/zudoku/src/lib/ui/Checkbox.tsx
+++ packages/zudoku/src/lib/ui/Checkbox.tsx
@@ -6,7 +6,7 @@ import * as React from "react";
 import { cn } from "../util/cn.js";
 
 const checkboxVariants = cva(
-  "peer h-4 w-4 shrink-0 rounded-sm ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:text-primary",
+  "peer h-4 w-4 shrink-0 rounded-[4px] ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:text-primary",
   {
     variants: {
       variant: {
