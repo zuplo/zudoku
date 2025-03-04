ddafafd8f1f57bcef6c14d98993bc55ef9bf786b
Daniel Lehr <daniellehr@gmx.de>
2025-03-04 11:17:29 +0100

    Update styles

--- packages/zudoku/src/lib/plugins/openapi/OperationListItem.tsx
+++ packages/zudoku/src/lib/plugins/openapi/OperationListItem.tsx
@@ -35,31 +35,28 @@ export const OperationListItem = ({
   const [selectedResponse, setSelectedResponse] = useState(first?.statusCode);
 
   return (
-    <div
-      key={operation.operationId}
-      className="grid grid-cols-1 lg:grid-cols-[minmax(0,4fr)_minmax(0,3fr)] gap-8 items-start border-b-2 mb-16 pb-16"
-    >
-      <div className="flex flex-col gap-4">
+    <div className="border-b-2 mb-16 pb-16">
+      {operation.deprecated && (
+        <Badge variant="muted" className="text-xs mb-4">
+          deprecated
+        </Badge>
+      )}
+      <div
+        key={operation.operationId}
+        className={cn(
+          "grid grid-cols-1 lg:grid-cols-[minmax(0,4fr)_minmax(0,3fr)] gap-x-8 gap-y-4 items-start",
+          operation.deprecated && "opacity-50 transition hover:opacity-100",
+        )}
+      >
         <Heading
           level={2}
           id={operation.slug}
           registerSidebarAnchor
           className="break-all"
         >
-          <span
-            className={cn(
-              operation.deprecated && "line-through text-muted-foreground",
-            )}
-          >
-            {operation.summary}
-          </span>
-          {operation.deprecated && (
-            <Badge variant="outline" className="ms-2 text-xs align-middle">
-              deprecated
-            </Badge>
-          )}
+          {operation.summary}
         </Heading>
-        <div className="text-sm flex gap-2 font-mono">
+        <div className="text-sm flex gap-2 font-mono col-span-full">
           <span className={methodForColor(operation.method)}>
             {operation.method.toUpperCase()}
           </span>
@@ -86,98 +83,101 @@ export const OperationListItem = ({
             </div>
           </div>
         </div>
-        {operation.description && (
-          <Markdown
-            className={`${ProseClasses} max-w-full prose-img:max-w-prose`}
-            content={operation.description}
-          />
-        )}
-        {operation.parameters && operation.parameters.length > 0 && (
-          <>
-            {PARAM_GROUPS.flatMap((group) =>
-              groupedParameters[group]?.length ? (
-                <ParameterList
-                  key={group}
-                  summary={operation.summary ?? undefined}
-                  id={operation.slug}
-                  parameters={groupedParameters[group]}
-                  group={group}
-                />
-              ) : (
-                []
-              ),
-            )}
-          </>
-        )}
-        {renderIf(operation.requestBody?.content?.at(0)?.schema, (schema) => (
-          <div className="mt-4 flex flex-col gap-4">
-            <Heading
-              level={3}
-              className="capitalize"
-              id={`${operation.slug}/request-body`}
-            >
-              {operation.summary && (
-                <VisuallyHidden>{operation.summary} &rsaquo; </VisuallyHidden>
+
+        <div className="flex flex-col gap-4">
+          {operation.description && (
+            <Markdown
+              className={`${ProseClasses} max-w-full prose-img:max-w-prose`}
+              content={operation.description}
+            />
+          )}
+          {operation.parameters && operation.parameters.length > 0 && (
+            <>
+              {PARAM_GROUPS.flatMap((group) =>
+                groupedParameters[group]?.length ? (
+                  <ParameterList
+                    key={group}
+                    summary={operation.summary ?? undefined}
+                    id={operation.slug}
+                    parameters={groupedParameters[group]}
+                    group={group}
+                  />
+                ) : (
+                  []
+                ),
               )}
-              Request Body
-            </Heading>
-            <SchemaView schema={schema} />
-          </div>
-        ))}
-        {operation.responses.length > 0 && (
-          <>
-            <Heading
-              level={3}
-              className="capitalize mt-8 pt-8 border-t"
-              id={`${operation.slug}/responses`}
-            >
-              {operation.summary && (
-                <VisuallyHidden>{operation.summary} &rsaquo; </VisuallyHidden>
-              )}
-              Responses
-            </Heading>
-            <Tabs
-              onValueChange={(value) => setSelectedResponse(value)}
-              value={selectedResponse}
-            >
-              {operation.responses.length > 1 && (
-                <TabsList>
+            </>
+          )}
+          {renderIf(operation.requestBody?.content?.at(0)?.schema, (schema) => (
+            <div className="mt-4 flex flex-col gap-4">
+              <Heading
+                level={3}
+                className="capitalize"
+                id={`${operation.slug}/request-body`}
+              >
+                {operation.summary && (
+                  <VisuallyHidden>{operation.summary} &rsaquo; </VisuallyHidden>
+                )}
+                Request Body
+              </Heading>
+              <SchemaView schema={schema} />
+            </div>
+          ))}
+          {operation.responses.length > 0 && (
+            <>
+              <Heading
+                level={3}
+                className="capitalize mt-8 pt-8 border-t"
+                id={`${operation.slug}/responses`}
+              >
+                {operation.summary && (
+                  <VisuallyHidden>{operation.summary} &rsaquo; </VisuallyHidden>
+                )}
+                Responses
+              </Heading>
+              <Tabs
+                onValueChange={(value) => setSelectedResponse(value)}
+                value={selectedResponse}
+              >
+                {operation.responses.length > 1 && (
+                  <TabsList>
+                    {operation.responses.map((response) => (
+                      <TabsTrigger
+                        value={response.statusCode}
+                        key={response.statusCode}
+                        title={response.description ?? undefined}
+                      >
+                        {response.statusCode}
+                      </TabsTrigger>
+                    ))}
+                  </TabsList>
+                )}
+                <ul className="list-none m-0 px-0">
                   {operation.responses.map((response) => (
-                    <TabsTrigger
+                    <TabsContent
                       value={response.statusCode}
                       key={response.statusCode}
-                      title={response.description ?? undefined}
                     >
-                      {response.statusCode}
-                    </TabsTrigger>
+                      <SchemaView
+                        schema={
+                          response.content?.find((content) => content.schema)
+                            ?.schema
+                        }
+                      />
+                    </TabsContent>
                   ))}
-                </TabsList>
-              )}
-              <ul className="list-none m-0 px-0">
-                {operation.responses.map((response) => (
-                  <TabsContent
-                    value={response.statusCode}
-                    key={response.statusCode}
-                  >
-                    <SchemaView
-                      schema={
-                        response.content?.find((content) => content.schema)
-                          ?.schema
-                      }
-                    />
-                  </TabsContent>
-                ))}
-              </ul>
-            </Tabs>
-          </>
-        )}
-      </div>
+                </ul>
+              </Tabs>
+            </>
+          )}
+        </div>
 
-      <Sidecar
-        selectedResponse={selectedResponse}
-        onSelectResponse={setSelectedResponse}
-        operation={operation}
-      />
+        <Sidecar
+          selectedResponse={selectedResponse}
+          onSelectResponse={setSelectedResponse}
+          operation={operation}
+        />
+      </div>
     </div>
   );
 };
