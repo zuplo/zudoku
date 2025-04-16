8d23bef690982a6d07dd601017b572a262b6413f
Daniel Lehr <daniellehr@gmx.de>
2025-04-16 15:43:04 +0200

    Build check

--- packages/zudoku/src/lib/components/BuildCheck.tsx
+++ packages/zudoku/src/lib/components/BuildCheck.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { LoaderCircleIcon } from "lucide-react";
+import { CircleFadingArrowUpIcon, LoaderCircleIcon } from "lucide-react";
 import { Button } from "../ui/Button.js";
 
 export const BuildCheck = ({
@@ -21,24 +21,38 @@ export const BuildCheck = ({
       }>,
   });
 
-  if (buildStatusQuery.data?.status === "completed") {
-    return null;
-  }
-
   if (buildStatusQuery.data?.buildId === buildId) {
     return null;
   }
 
+  const isCompleted = buildStatusQuery.data?.status === "completed";
+
   return (
-    <div className="fixed p-4 rounded-xl w-92 border z-20 bg-muted text-muted-foreground left-0 right-0 top-4 mx-auto shadow-lg">
-      <div className="flex flex-row items-center gap-2">
-        <LoaderCircleIcon size={16} className="animate-spin" />
-        <span className="text-sm">Building new version...</span>
-      </div>
+    <div className="fixed flex flex-col gap-3 p-4 rounded-xl w-96 border z-20 bg-background left-0 right-0 top-4 mx-auto shadow-lg">
+      {isCompleted ? (
+        <div className="flex flex-row items-center gap-2">
+          <CircleFadingArrowUpIcon size={16} />
+          <span className="text-sm">New version available</span>
+        </div>
+      ) : (
+        <div className="flex flex-row items-center gap-2">
+          <LoaderCircleIcon size={16} className="animate-spin" />
+          <span className="text-sm">Building new version...</span>
+        </div>
+      )}
       <span className="text-xs">
-        A new version of this will be available in a few minutes.
+        {!isCompleted
+          ? "A new version of the developer portal will be available soon."
+          : "To see the new version, reload the page now."}
       </span>
-      <Button variant="outline" size="sm" className="w-full">
+      <Button
+        variant="outline"
+        size="sm"
+        className="w-full"
+        onClick={() => {
+          window.location.reload();
+        }}
+      >
         Reload
       </Button>
     </div>
