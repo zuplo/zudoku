718e9b74672f5338cc8b9504705eae07b1298672
Daniel Lehr <daniellehr@gmx.de>
2025-04-27 16:16:35 +0200

    Load in SSR

--- packages/zudoku/src/vite/plugin-shiki-register.ts
+++ packages/zudoku/src/vite/plugin-shiki-register.ts
@@ -1,7 +1,11 @@
+import type { BundledLanguage } from "shiki";
 import type { Plugin } from "vite";
 import type { LoadedConfig } from "../config/config.js";
-import { defaultHighlightOptions, defaultLanguages } from "../lib/shiki.js";
-
+import {
+  defaultHighlightOptions,
+  defaultLanguages,
+  highlighter,
+} from "../lib/shiki.js";
 export const viteShikiRegisterPlugin = (
   getCurrentConfig: () => LoadedConfig,
 ): Plugin => {
@@ -15,24 +19,26 @@ export const viteShikiRegisterPlugin = (
         return resolvedVirtualModuleId;
       }
     },
-    api: {},
-    load(id) {
+    async load(id) {
       if (id !== resolvedVirtualModuleId) return;
       const config = getCurrentConfig();
 
       const languages =
         config.syntaxHighlighting?.languages ?? defaultLanguages;
 
+      const themes = config.syntaxHighlighting?.themes;
+      const lightTheme = themes?.light ?? defaultHighlightOptions.themes.light;
+      const darkTheme = themes?.dark ?? defaultHighlightOptions.themes.dark;
+
+      await highlighter.loadTheme(lightTheme, darkTheme);
+      await highlighter.loadLanguage(...(languages as BundledLanguage[]));
+
       const code = [
         "export const registerShiki = async (highlighter) => {",
-        "  const promises = [",
-        `    highlighter.loadTheme('${config.syntaxHighlighting?.themes?.light ?? defaultHighlightOptions.themes.light}'),`,
-        `    highlighter.loadTheme('${config.syntaxHighlighting?.themes?.dark ?? defaultHighlightOptions.themes.dark}'),`,
-        ...languages.map(
-          (language) => `highlighter.loadLanguage('${language}'),`,
-        ),
-        "  ];",
-        "  await Promise.all(promises);",
+        `  await Promise.all([`,
+        `    highlighter.loadTheme("${lightTheme}", "${darkTheme}"),`,
+        `    highlighter.loadLanguage(...${JSON.stringify(languages)}),`,
+        "  ]);",
         "};",
       ];
 
