b4f1f4e6e38bc0b81b714ea8e89187f4a07d0099
Daniel Lehr <daniellehr@gmx.de>
2025-05-20 09:03:32 +0200

    Remove unused `esm-loader-css`

--- packages/zudoku/cli.js
+++ packages/zudoku/cli.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-import { register } from "node:module";
-register("esm-loader-css", import.meta.url);
-
 if (process.env.ZUDOKU_INTERNAL_CLI) {
   await import("./scripts/cli-dev.js");
 } else {
