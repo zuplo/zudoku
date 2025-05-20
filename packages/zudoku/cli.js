ace2b5f0d62bc262d372d7adff24a7d09286d31f
Dan <dan@zuplo.com>
2025-05-20 16:28:51 +0200

    Check Node version (#1017)

--- packages/zudoku/cli.js
+++ packages/zudoku/cli.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 
-import { register } from "node:module";
-register("esm-loader-css", import.meta.url);
+import semver from "semver";
+
+if (!semver.satisfies(process.version, ">=20.19.0 <21.0.0 || >=22.7.0")) {
+  console.error(
+    `⚠️ Zudoku requires Node.js version >=20.19.0 or >=22.7.0. Your version: ${process.version}`,
+  );
+  process.exit(1);
+}
 
 if (process.env.ZUDOKU_INTERNAL_CLI) {
   await import("./scripts/cli-dev.js");
