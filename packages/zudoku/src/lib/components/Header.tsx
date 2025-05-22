11494fd0d0b16ff3f02c2d49cdf1ecb030932d2a
Daniel Lehr <daniellehr@gmx.de>
2025-05-22 19:47:06 +0200

    Fix phantom scroll issue

--- packages/zudoku/src/lib/components/Header.tsx
+++ packages/zudoku/src/lib/components/Header.tsx
@@ -19,6 +19,7 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "../ui/DropdownMenu.js";
+import { cn } from "../util/cn.js";
 import { joinUrl } from "../util/joinUrl.js";
 import { Banner } from "./Banner.js";
 import { ClientOnly } from "./ClientOnly.js";
@@ -76,10 +77,12 @@ export const Header = memo(function HeaderInner() {
       : joinUrl(options.basePath, page.logo.src.dark)
     : undefined;
 
+  const borderBottom = "inset-shadow-[0_-1px_0_0_var(--border)]";
+
   return (
     <header className="sticky lg:top-0 z-10 bg-background/80 backdrop-blur-xs w-full">
       <Banner />
-      <div className="border-b">
+      <div className={borderBottom}>
         <div className="max-w-screen-2xl mx-auto flex relative items-center justify-between px-4 lg:px-8 h-(--top-header-height) border-transparent">
           <div className="flex">
             <Link to="/">
@@ -90,14 +93,14 @@ export const Header = memo(function HeaderInner() {
                       src={logoLightSrc}
                       alt={page.logo.alt ?? page.pageTitle}
                       style={{ width: page.logo.width }}
-                      className="max-h-[--top-header-height] dark:hidden"
+                      className="max-h-(--top-header-height) dark:hidden"
                       loading="lazy"
                     />
                     <img
                       src={logoDarkSrc}
                       alt={page.logo.alt ?? page.pageTitle}
                       style={{ width: page.logo.width }}
-                      className="max-h-[--top-header-height] hidden dark:block"
+                      className="max-h-(--top-header-height) hidden dark:block"
                       loading="lazy"
                     />
                   </>
@@ -178,7 +181,7 @@ export const Header = memo(function HeaderInner() {
           </div>
         </div>
       </div>
-      <div className="border-b hidden lg:block">
+      <div className={cn("hidden lg:block", borderBottom)}>
         <div className="max-w-screen-2xl mx-auto border-transparent">
           <Slot.Target name="top-navigation-before" />
           <TopNavigation />
