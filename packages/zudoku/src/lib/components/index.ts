036c2114ce2af8d984da79a2b6de3f09b23e207b
Dan <dan@zuplo.com>
2025-05-28 15:40:26 +0200

    Add deprecated

--- packages/zudoku/src/lib/components/index.ts
+++ packages/zudoku/src/lib/components/index.ts
@@ -1,7 +1,9 @@
 import { useMDXComponents as useMDXComponentsImport } from "@mdx-js/react";
 import { Helmet } from "@zudoku/react-helmet-async";
+/** @deprecated Import from `zudoku/hooks` instead */
 import { useTheme as useThemeImport } from "next-themes";
 import { Link as LinkImport } from "react-router";
+/** @deprecated Import from `zudoku/hooks` instead */
 import { useAuth as useAuthImport } from "../authentication/hook.js";
 import { RouteGuard as RouteGuardImport } from "../core/RouteGuard.js";
 import { RouterError as RouterErrorImport } from "../errors/RouterError.js";
@@ -13,11 +15,13 @@ import {
   BootstrapStatic as BootstrapStaticImport,
 } from "./Bootstrap.js";
 import { BuildCheck as BuildCheckImport } from "./BuildCheck.js";
+/** @deprecated Import from `zudoku/hooks` instead */
 import {
   CACHE_KEYS as CACHE_KEYS_IMPORT,
   useCache as useCacheImport,
 } from "./cache.js";
 import { ClientOnly as ClientOnlyImport } from "./ClientOnly.js";
+/** @deprecated Import from `zudoku/hooks` instead */
 import { useZudoku as useZudokuImport } from "./context/ZudokuContext.js";
 import { Layout as LayoutImport } from "./Layout.js";
 import { Markdown as MarkdownImport } from "./Markdown.js";
@@ -25,6 +29,7 @@ import { Spinner as SpinnerImport } from "./Spinner.js";
 import { StatusPage as StatusPageImport } from "./StatusPage.js";
 import { Zudoku as ZudokuImport } from "./Zudoku.js";
 
+/** @deprecated Import from `zudoku/hooks` instead */
 export const useMDXComponents = /*@__PURE__*/ useMDXComponentsImport;
 export const Layout = /*@__PURE__*/ LayoutImport;
 export const RouterError = /*@__PURE__*/ RouterErrorImport;
@@ -35,9 +40,13 @@ export const RouteGuard = /*@__PURE__*/ RouteGuardImport;
 
 export const Head = /*@__PURE__*/ Helmet;
 
+/** @deprecated Import from `zudoku/hooks` instead */
 export const useZudoku = /*@__PURE__*/ useZudokuImport;
+/** @deprecated Import from `zudoku/hooks` instead */
 export const useAuth = /*@__PURE__*/ useAuthImport;
+/** @deprecated Import from `zudoku/hooks` instead */
 export const useCache = /*@__PURE__*/ useCacheImport;
+/** @deprecated Import from `zudoku/hooks` instead */
 export const CACHE_KEYS = /*@__PURE__*/ CACHE_KEYS_IMPORT;
 export const Zudoku = /*@__PURE__*/ ZudokuImport;
 
@@ -48,5 +57,6 @@ export const Spinner = /*@__PURE__*/ SpinnerImport;
 export const ClientOnly = /*@__PURE__*/ ClientOnlyImport;
 export const Button = /*@__PURE__*/ ButtonImport;
 export const Link = /*@__PURE__*/ LinkImport;
+/** @deprecated Import from `zudoku/hooks` instead */
 export const useTheme = /*@__PURE__*/ useThemeImport;
 export const BuildCheck = /*@__PURE__*/ BuildCheckImport;
