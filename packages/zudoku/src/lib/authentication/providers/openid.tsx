19ffade60ec380f944189655e03e2cf09f2da4b8
Moritz <moritz@schwrr.com>
2025-06-05 06:39:53 +0200

    fix formatting

--- packages/zudoku/src/lib/authentication/providers/openid.tsx
+++ packages/zudoku/src/lib/authentication/providers/openid.tsx
@@ -308,7 +308,7 @@ export class OpenIDAuthenticationProvider
         );
 
         if (!response.access_token) {
-          throw new AuthorizationError('No access token in response');
+          throw new AuthorizationError("No access token in response");
         }
 
         this.setTokensFromResponse(response);
