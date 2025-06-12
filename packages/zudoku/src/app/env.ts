1cb87c341c476453a95b3b6357f7e84e6eda2886
Moritz <moritz@schwrr.com>
2025-06-12 07:44:47 +0200

    improve validation & reporting

--- packages/zudoku/src/app/env.ts
+++ packages/zudoku/src/app/env.ts
@@ -1,14 +1,22 @@
+import z from "zod";
 import { BuildConfigSchema } from "./ZuploBuildConfig.js";
 
 const getZuploBuildConfig = () => {
   if (!process.env.ZUPLO_BUILD_CONFIG) return undefined;
 
   try {
-    const zuploBuildConfig = BuildConfigSchema.parse(
+    const zuploBuildConfig = BuildConfigSchema.safeParse(
       JSON.parse(process.env.ZUPLO_BUILD_CONFIG),
     );
     return zuploBuildConfig;
   } catch (error) {
+    if (error instanceof z.ZodError) {
+      // eslint-disable-next-line no-console
+      console.error("ZUPLO_BUILD_CONFIG is invalid.");
+      // eslint-disable-next-line no-console
+      console.log(error.errors);
+      return undefined;
+    }
     // eslint-disable-next-line no-console
     console.error(
       "ZUPLO_BUILD_CONFIG is a reserved environment variable and cannot be used for custom configuration. Please remove it from your environment variables.",
