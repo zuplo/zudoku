e57638dc060d28ade3315fdebac6fe14648a4f80
Moritz <moritz@schwrr.com>
2025-06-13 16:46:26 +0200

    use consistent labels (#1199)

--- packages/zudoku/src/lib/plugins/api-keys/index.tsx
+++ packages/zudoku/src/lib/plugins/api-keys/index.tsx
@@ -143,7 +143,8 @@ const createDefaultHandler = (deploymentName: string): ApiKeyService => {
         data: [
           {
             id: string;
-            label: string;
+            label?: string;
+            subject?: string;
             apiKeys: {
               data: ApiKey[];
             };
@@ -153,7 +154,7 @@ const createDefaultHandler = (deploymentName: string): ApiKeyService => {
 
       return data.data.map((consumer) => ({
         id: consumer.id,
-        label: consumer.label || "API Key",
+        label: consumer.label ?? consumer.subject ?? "API Key",
         apiKeys: consumer.apiKeys.data,
         key: consumer.apiKeys.data.at(0),
       }));
@@ -195,7 +196,7 @@ export const apiKeyPlugin = (
             return request;
           },
           id: consumer.id,
-          label: consumer.description ?? consumer.id,
+          label: consumer.label,
         }));
       } catch {
         return [];
