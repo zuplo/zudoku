0cdd9670258936919216392554efdc0b4576fb6c
Moritz <moritz@schwrr.com>
2025-07-15 19:24:13 +0200

    [OpenAPI] Add support for nested categories using `x-tagGroups` (#1335)
    Co-authored-by: Daniel Lehr <daniellehr@gmx.de>

--- packages/zudoku/src/lib/plugins/openapi/graphql/graphql.ts
+++ packages/zudoku/src/lib/plugins/openapi/graphql/graphql.ts
@@ -383,6 +383,7 @@ export type GetNavigationOperationsQuery = {
   __typename?: "Query";
   schema: {
     __typename?: "Schema";
+    extensions?: any | null;
     tags: Array<{
       __typename?: "SchemaTag";
       slug?: string | null;
@@ -645,6 +646,7 @@ export const GetServerQueryDocument = new TypedDocumentString(`
 export const GetNavigationOperationsDocument = new TypedDocumentString(`
     query GetNavigationOperations($input: JSON!, $type: SchemaType!) {
   schema(input: $input, type: $type) {
+    extensions
     tags {
       slug
       name
