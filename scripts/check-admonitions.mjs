9d2cbb6534e381a05eb91b1d9432c03d858e0b93
Moritz <moritz@schwrr.com>
2025-07-18 11:38:58 +0200

    ok

--- scripts/check-admonitions.mjs
+++ scripts/check-admonitions.mjs
@@ -37,7 +37,7 @@ const globPatterns =
     ? args
     : ["docs/pages/**/*.{md,mdx}", "examples/**/pages/**/*.{md,mdx}"];
 
-const IGNORE_FILES = ["docs/pages/docs/markdown/admonitions.mdx"];
+const IGNORE_FILES = ["docs/pages/docs/markdown/admonitions.md"];
 
 let totalIssues = 0;
 const issues = [];
