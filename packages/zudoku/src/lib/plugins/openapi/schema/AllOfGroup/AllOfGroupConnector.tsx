51ddb2160a5e4b9292cba1d82072e6de49863631
Daniel Lehr <daniellehr@gmx.de>
2025-07-28 14:43:41 +0200

    Improve rendering of `anyOf` and `oneOf`

--- packages/zudoku/src/lib/plugins/openapi/schema/AllOfGroup/AllOfGroupConnector.tsx
+++ packages/zudoku/src/lib/plugins/openapi/schema/AllOfGroup/AllOfGroupConnector.tsx
@@ -1,32 +1,11 @@
-import {
-  ChevronDownIcon,
-  CircleDotIcon,
-  CircleFadingPlusIcon,
-  CircleIcon,
-} from "lucide-react";
+import { ChevronDownIcon, CircleFadingPlusIcon } from "lucide-react";
 import { cn } from "../../../../util/cn.js";
 
-import type { LogicalGroupType } from "../utils.js";
-
-const iconMap = {
-  AND: <CircleFadingPlusIcon size={16} className="fill-card" />,
-  OR: <CircleDotIcon size={16} className="fill-card" />,
-  ONE: <CircleIcon size={14} className="fill-card" />,
-} as const;
-
-const colorClass = {
-  AND: "text-green-500 dark:text-green-300/60",
-  OR: "text-blue-400 dark:text-blue-500",
-  ONE: "text-purple-500 dark:text-purple-300/60",
-} as const;
-
-export const LogicalGroupConnector = ({
-  type,
+export const AllOfGroupConnector = ({
   isOpen,
   className,
   schemeName,
 }: {
-  type: LogicalGroupType;
   isOpen: boolean;
   className?: string;
   schemeName?: string;
@@ -34,14 +13,14 @@ export const LogicalGroupConnector = ({
   return (
     <div
       className={cn(
-        colorClass[type],
+        "text-green-500 dark:text-green-300/60",
         "relative text-sm flex py-2",
         "before:border-l before:absolute before:-top-2 before:-bottom-2 before:border-border before:border-dashed before:content-['']",
         className,
       )}
     >
       <div className="-translate-x-[7px] flex gap-1 items-center">
-        {iconMap[type]}
+        <CircleFadingPlusIcon size={16} className="fill-card" />
         <div
           className={cn(
             "translate-y-px mx-px opacity-0 group-hover:opacity-100 transition",
---
+++ packages/zudoku/src/lib/plugins/openapi/schema/AllOfGroup/AllOfGroupView.tsx
@@ -0,0 +1,35 @@
+import * as Collapsible from "@radix-ui/react-collapsible";
+import { SquareMinusIcon, SquarePlusIcon } from "lucide-react";
+import { useCallback, useState } from "react";
+import type { SchemaObject } from "../../../../oas/parser/index.js";
+import { Card } from "../../../../ui/Card.js";
+import { AllOfGroupItem } from "./AllOfGroupItem.js";
+
+export const AllOfGroupView = ({ schema }: { schema: SchemaObject }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const toggleOpen = useCallback(() => setIsOpen((prev) => !prev), []);
+
+  if (!schema.allOf) return null;
+
+  return (
+    <Collapsible.Root open={isOpen} onOpenChange={toggleOpen} asChild>
+      <Card className="px-6">
+        <Collapsible.Trigger className="flex gap-2 items-center py-2 w-full text-sm text-muted-foreground -translate-x-1.5">
+          {isOpen ? (
+            <SquareMinusIcon size={14} />
+          ) : (
+            <SquarePlusIcon size={14} />
+          )}
+          <span>All of</span>
+        </Collapsible.Trigger>
+
+        <Collapsible.Content className="pb-4">
+          {schema.allOf.map((subSchema, index) => (
+            // biome-ignore lint/suspicious/noArrayIndexKey: Index needed for key
+            <AllOfGroupItem key={index} schema={subSchema} />
+          ))}
+        </Collapsible.Content>
+      </Card>
+    </Collapsible.Root>
+  );
+};
