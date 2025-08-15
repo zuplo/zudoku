f816fd197a88480f0275e537101a81114636be33
Daniel Lehr <daniellehr@gmx.de>
2025-08-15 09:09:56 +0200

    Fix formatting

--- packages/zudoku/src/lib/plugins/openapi/OperationList.tsx
+++ packages/zudoku/src/lib/plugins/openapi/OperationList.tsx
@@ -220,7 +220,10 @@ export const OperationList = ({
     next: next
       ? {
           to: `../${next.slug ?? UNTAGGED_PATH}`,
-          label: next.extensions?.["x-displayName"] ?? next.name ?? "Other endpoints",
+          label:
+            next.extensions?.["x-displayName"] ??
+            next.name ??
+            "Other endpoints",
         }
       : undefined,
   };
