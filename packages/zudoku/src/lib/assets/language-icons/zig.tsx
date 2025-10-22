cd5c68649a183f53ec13f505d9f14e74f4db2d93
David Barrat <david@barrat.io>
2025-10-22 10:45:12 +0200

    Add support for additional programming languages (#1614)
    Co-authored-by: Daniel Lehr <daniellehr@gmx.de>

---
+++ packages/zudoku/src/lib/assets/language-icons/zig.tsx
@@ -0,0 +1,32 @@
+import type { SVGProps } from "react";
+
+const ZigIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 400 140"
+    {...props}
+  >
+    <title>Zig</title>
+    <g fill="#F7A41D">
+      <path d="M46 22 28 44l-9-14z" />
+      <path d="M46 22 33 33l-5 11h-6v51h9l-11 5-8 17H0V22z" />
+      <path d="m31 95-19 22-8-11zM56 22l6 14-25 8z" />
+      <path d="M56 22h55v22H37l19-12zM116 95l-19 22-7-13z" />
+      <path d="m116 95-16 9-3 13H42V95z" />
+      <path d="M150 0 52 117 3 140l98-118zM141 22l-1 18-18 5z" />
+      <path d="M153 22v95h-47l14-12 5-10h6V45h-9l10-9 9-14z" />
+      <path d="m125 95 5 15-24 7z" />
+    </g>
+    <g fill="currentColor">
+      <path d="M260 22v15l-31 3h-52V22z" />
+      <path d="m260 37-53 62v4h-31l53-63v-3z" />
+      <path d="M261 99v18h-85v-14l30-4zM272 22h22v95h-22zM394 67v39h-18V81l-16-11-14-3z" />
+      <path d="m360 68 16 13-30-14z" />
+      <path d="M394 106c-10.2 7.3-24 12-37.7 12-29 0-51.1-20.8-51.1-48.3 0-27.3 22.5-48.1 52-48.1 14.3 0 29.2 5.5 38.9 14l-13 15c-7.1-6.3-16.8-10-25.9-10-17 0-30.2 12.9-30.2 29.5 0 16.8 13.3 29.6 30.3 29.6 5.7 0 12.8-2.3 19-5.5L394 106z" />
+    </g>
+  </svg>
+);
+
+export default ZigIcon;
