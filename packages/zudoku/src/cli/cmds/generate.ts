5fde799c2e43dc2b77c6014474b0abae8ef9be2f
zeudev <coding@zeu.dev>
2025-11-20 15:16:49 -0800

    add generate to cli, init examples as subcommand, add generate to with-openapi-json for debug

---
+++ packages/zudoku/src/cli/cmds/generate.ts
@@ -0,0 +1,60 @@
+import type { Argv } from "yargs";
+
+export default {
+  desc: "Generate additions to the OpenAPI schema manually or using AI",
+  command: "generate",
+  builder: (yargs: Argv): Argv<unknown> => {
+    return yargs.command(examplesSubCommand);
+    // add more options for generation as sub commands
+  },
+};
+
+export interface ExamplesArguments {
+  schema: string;
+  paths?: string[];
+  mode?: "ai" | "local" | "manual";
+}
+
+const examplesSubCommand = {
+  desc: "Generate examples for paths with no request body or response examples",
+  command: "examples",
+  builder: (yargs: Argv): Argv<unknown> => {
+    return yargs
+      .option("schema", {
+        type: "string",
+        alias: "s",
+        describe: "The OpenAPI schema file, either YAML or JSON",
+        demandOption:
+          "Please provide the OpenAPI schema file path to run this command on using --schema or -s",
+      })
+      .option("paths", {
+        type: "array",
+        alias: "p",
+        describe:
+          "Specific API endpoints to generate examples for (e.g. /todo)",
+      })
+      .option("mode", {
+        type: "string",
+        alias: "m",
+        choices: ["ai", "local", "manual"],
+        describe:
+          "How Zudoku will generate the examples (AI, local code generation, manual user input)",
+      });
+  },
+  handler: async (argv: unknown) => {
+    /** TODO: uncomment for analytics
+    await captureEvent({
+      argv,
+      event: "zudoku dev",
+    });
+    **/
+    await examplesHandler(argv as ExamplesArguments);
+  },
+};
+
+async function examplesHandler(argv: ExamplesArguments) {
+  console.log("GENERATE");
+  console.log(argv.schema);
+  console.log(argv.paths);
+  console.log(argv.mode);
+}
