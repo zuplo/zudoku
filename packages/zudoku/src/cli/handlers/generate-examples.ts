5f6a105ffc841f4732aa56ed2c0fa270584ed707
zeudev <coding@zeu.dev>
2025-12-02 12:18:00 -0800

    check for existing explicit examples

--- packages/zudoku/src/cli/handlers/generate-examples.ts
+++ packages/zudoku/src/cli/handlers/generate-examples.ts
@@ -7,38 +7,58 @@ export async function examplesHandler(argv: ExamplesArguments) {
   console.log({ schemaFile, paths, mode });
 
   const needsExamplePaths: { node: RecordAny, path: string[] }[] = []
-  function needsExample(node: RecordAny, path: string[]) {
+
+  type TraverseTransformProps = {
+    node: RecordAny;
+    path: string[] | undefined;
+  };
+
+  function needsExample({ node, path, schema }: TraverseTransformProps & { schema: RecordAny }) {
+    // Multiple ways to have examples show up to RequestBody/Responses via "content.type"
+    // - having a schema with $ref to a component that has example defined per property
+    // - "example" object with properties (not necessarily have to be inline w schema)
+    // - "examples" with named keys (eg { "domestic": {...}, "international": {...} })
+
     // check if the path has a request body or responses
-    if (path.includes("requestBody") || path.includes("responses")) {
+    if (path && (path.includes("requestBody") || path.includes("responses"))) {
       // check if the current [1] endpoint and [2] http method is accounted for
       if (!(needsExamplePaths.filter((p) => p.path[1] === path[1] && p.path[2] === path[2]).length > 0)) {
-        // check if current node path has a schema to work on
-        if (path.includes("schema")) {
-          needsExamplePaths.push({ node, path });
+
+        // check if current node path has a schema to work on and no examples
+        if (path.indexOf("schema") === path.length - 1) {
+
+          // check if explicit examples already exist
+          const existingExample = path.toSpliced(path.length-1, 1, "example").reduce((current, key) => {
+            return current?.[key];
+          }, schema);
+          const existingExamples = path.toSpliced(path.length-1, 1, "examples").reduce((current, key) => {
+            return current?.[key];
+          }, schema);
+
+          if (!existingExample && !existingExamples) {
+            needsExamplePaths.push({ node, path });
+            return true;
+          }
         }
       }
     }
+    return false;
+  }
+
+  function writeExample({ node, path }: TraverseTransformProps) {
+    return node;
   }
 
   if (schemaFile.endsWith(".json")) {
     const json = fs.readFileSync(schemaFile, "utf-8");
     const schema = JSON.parse(json);
 
-    // Multiple ways to have examples show up to RequestBody via "content.type"
-    // - having a schema with $ref to a component that has example defined per property
-    // - "example" object with properties (not necessarily have to be inline w schema)
-    // - "examples" with named keys (eg { "domestic": {...}, "international": {...} })
-
-    // 1. find endpoints without explicit example(s)
     traverse(schema, (node, path) => {
-      // if the current node is a path
+      // 1. find endpoints without explicit example(s)
       if (path?.includes("paths")) {
-        needsExample(node, path)
+        needsExample({ node, path, schema }) ? console.log({ node, path }) : node;
       }
       return node;
     });
-
-
-    console.log(needsExamplePaths);
   }
 }
