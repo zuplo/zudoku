98d85d41828aaa79f66a888c8ad565afa2fbdbd4
Keshav Aneja <keshav@motorq.com>
2026-01-23 13:08:56 +0530

    Spread userInfo into profile object
    With the Auth0 provider, additional fields such as app_metadata or user_metadata returned from the /userinfo endpoint are not being mapped to the profile object, despite being included in the userinfo response.

--- packages/zudoku/src/lib/authentication/providers/openid.tsx
+++ packages/zudoku/src/lib/authentication/providers/openid.tsx
@@ -424,6 +424,7 @@ export class OpenIDAuthenticationProvider
     const userInfo = await userInfoResponse.json();
 
     const profile: UserProfile = {
+      ...userInfo,
       sub: userInfo.sub,
       email: userInfo.email,
       name: userInfo.name,
