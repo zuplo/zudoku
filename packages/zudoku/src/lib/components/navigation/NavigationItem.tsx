d19d4feded246e0664fe09585d3556581dce56e5
Eivind Grimstad <eivind.grimstad@knowit.no>
2026-01-29 14:54:31 +0100

    fix: link/custom-page without anchor is not highlighted

--- packages/zudoku/src/lib/components/navigation/NavigationItem.tsx
+++ packages/zudoku/src/lib/components/navigation/NavigationItem.tsx
@@ -120,6 +120,7 @@ export const NavigationItem = ({
     case "link":
     case "custom-page": {
       const href = item.type === "link" ? item.to : joinUrl(item.path);
+      const hasAnchor = href.includes("#");
       return !href.startsWith("http") ? (
         <AnchorLink
           to={{
@@ -131,7 +132,9 @@ export const NavigationItem = ({
           className={navigationListItem({
             isActive:
               href ===
-              [location.pathname, activeAnchor].filter(Boolean).join("#"),
+              (hasAnchor
+                ? [location.pathname, activeAnchor].filter(Boolean).join("#")
+                : location.pathname),
           })}
           onClick={onRequestClose}
         >
