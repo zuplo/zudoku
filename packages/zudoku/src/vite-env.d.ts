0694aeeee540ff40597b7c94eaaa6b9546b89e2b
Dan <daniellehr@gmx.de>
2026-03-25 07:28:21 +0100

    Upgrade to Typescript 6

--- packages/zudoku/src/vite-env.d.ts
+++ packages/zudoku/src/vite-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 
+declare module "vite/modulepreload-polyfill" {}
+
 interface ImportMetaEnv {
   readonly IS_ZUPLO: boolean;
   readonly ZUPLO_BUILD_ID?: string;
